dd64df8b496a8e2bb60afa795b767bb31d4d16ff
KHALDOUN Mohsen <mohsenuss91@hotmail.com>
2024-07-18 18:27:15 +0100

    Update language.js
    Refactored code

--- js/language.js
+++ js/language.js
@@ -1,75 +1,61 @@
-
 const LANG_TABLE = {
-    // English
-    "en": {
-        LANGAUGE:     "Language :",
+    en: {
+        LANGUAGE: "Language :",
         LOAD_ENTRIES: "Load entries :",
         ENTRIES_LIST: "Entries list:",
-        START_ALL:    "Start all",
-        STOP_ALL:     "Stop all",
-        VIEW:         "Open",
-        START:        "Start",
-        STOP:         "Stop",
+        START_ALL: "Start all",
+        STOP_ALL: "Stop all",
+        VIEW: "Open",
+        START: "Start",
+        STOP: "Stop",
     },
-
-    // French
-    "fr": {
-        LANGAUGE:     "Langue :",
+    fr: {
+        LANGUAGE: "Langue :",
         LOAD_ENTRIES: "Charger les entrées :",
         ENTRIES_LIST: "Liste des entrées:",
-        START_ALL:    "Lancer tout",
-        STOP_ALL:     "Arrêter tous",
-        VIEW:         "Ouvrir",
-        START:        "Lancer",
-        STOP:         "Arrêtr",
+        START_ALL: "Lancer tout",
+        STOP_ALL: "Arrêter tous",
+        VIEW: "Ouvrir",
+        START: "Lancer",
+        STOP: "Arrêter",
     },
-
-    // Arabic
-    "ar": {
-        LANGAUGE:     "الغة :",
+    ar: {
+        LANGUAGE: "الغة :",
         LOAD_ENTRIES: "تحميل الإدخالات :",
         ENTRIES_LIST: "قائمة الإدخالات :",
-        START_ALL:    "بدا الجميع",
-        STOP_ALL:     "توقيف الجميع",
-        VIEW:         "افتح",
-        START:        "ابدا",
-        STOP:         "توقف",
+        START_ALL: "بدا الجميع",
+        STOP_ALL: "توقيف الجميع",
+        VIEW: "افتح",
+        START: "ابدا",
+        STOP: "توقف",
     },
 };
 
-let lang_id = "fr"; // default
-const language_select = document.getElementById( "langauge_selection" )
+let langId = "fr"; // default
+const languageSelect = document.getElementById("language_selection");
 
-chrome.storage.local.get( ["language"], (result) => {
-    let new_lang_id = "fr";
-    if( result.language !== undefined )
-        new_lang_id = result.language;
+chrome.storage.local.get(["language"], (result) => {
+    langId = result.language || "fr";
+    languageSelect.value = langId;
+    changeLanguage(langId);
+});
 
-    language_select.value = new_lang_id; // update select option 
-    ChangeLanguage( new_lang_id );
-} );
+languageSelect.onchange = (e) => changeLanguage(e.target.value);
 
-language_select.onchange = (e) => ChangeLanguage( e.target.value ) 
+function changeLanguage(newLangId) {
+    langId = newLangId;
+    chrome.storage.local.set({ language: newLangId });
 
-function ChangeLanguage( new_lang_id ) {
-    // presist data
-    lang_id = new_lang_id;
-    chrome.storage.local.set( { language: new_lang_id } );
+    document.querySelectorAll("[data-trans]").forEach((ele) => {
+        ele.innerHTML = translate(ele.dataset.trans);
+    });
 
-    // apply change
-    let elements = document.querySelectorAll( "[data-trans]" );
-    for( ele of elements )
-        ele.innerHTML = TRANSLATE( ele.dataset.trans ) 
-
-    // text direction
-    if( new_lang_id === "ar" ) {
-        // the whole body except "ol" container
-        document.body.style.direction = "rtl";   
-        document.getElementsByTagName( "ol" )[0].style.direction = "ltr"
-    } else 
-        document.body.style.direction = "ltr";   
+    document.body.style.direction = newLangId === "ar" ? "rtl" : "ltr";
+    if (newLangId === "ar") {
+        document.querySelector("ol").style.direction = "ltr";
+    }
 }
 
-function TRANSLATE( trans_id ) {
-    return LANG_TABLE[ lang_id ][ trans_id ];
+function translate(transId) {
+    return LANG_TABLE[langId][transId];
 }
